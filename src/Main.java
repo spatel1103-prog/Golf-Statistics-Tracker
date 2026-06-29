26a3312a3fd4166cadddda00f5b84635cbd23e6f
sarinapatel <sarinapatel1103@gmail.com>
2026-06-29 11:37:21 -0400

    Initial project setup

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,40 @@
 public class Main {
 
     public static void main (String [] args ) {
-        System.out.print("Hello Golf Tracker!");
+
+        GolfTracker tracker = new GolfTracker();
+
+        tracker.addRound(new GolfRound(
+                "Applebrook",
+                "Jun 28",
+                82,
+                31,
+                8,
+                10,
+                1));
+
+        tracker.addRound(new GolfRound(
+                "Merion",
+                "Jul 5",
+                79,
+                29,
+                10,
+                12,
+                0));
+
+        tracker.addRound(new GolfRound(
+                "Aronimink",
+                "Jul 12",
+                85,
+                33,
+                7,
+                8,
+                2));
+
+        tracker.displayRounds();
+
+
+
     }
 
 
